e1fbc5cb6ab89465bc9ed0f91a268a984a1a5f3a
jojonki <junki.ohmura@gmail.com>
2014-02-02 19:55:14 +0900

    add effect app

---
+++ effects/Assets/Scripts/MoveAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.U)) {
+			iTween.MoveTo(gameObject, iTween.Hash("y", 5f, "time", 0.1f, "easeType", "easeOutSine"));
+		} else if(Input.GetKey(KeyCode.LeftArrow)) {
+			gameObject.transform.localPosition = gameObject.transform.localPosition + new Vector3(-0.1f, 0f);
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			gameObject.transform.localPosition = gameObject.transform.localPosition + new Vector3(0.1f, 0f);
+		} else if(Input.GetKey(KeyCode.UpArrow)) {
+			gameObject.transform.localPosition = gameObject.transform.localPosition + new Vector3(0f, 0.1f);
+		} else if(Input.GetKey(KeyCode.DownArrow)) {
+			gameObject.transform.localPosition = gameObject.transform.localPosition + new Vector3(0f, -0.1f);
+		}
+//		gameObject.transform.localPosition = Input.mousePosition * 0.0.1f;
+	}
+}
---
+++ effects/Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(new Vector3(0f, Time.deltaTime * 50f, 0f));
+	}
+}
